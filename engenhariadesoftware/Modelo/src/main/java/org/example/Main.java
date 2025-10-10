17da26ada615b4bfcd191708905490d5986d272b
gabrielvalentesjc <gabrielvalentesjc@gmail.com>
2025-10-10 20:47:21 -0300

    Atualiza a atividade 8

--- engenhariadesoftware/Modelo/src/main/java/org/example/Main.java
+++ engenhariadesoftware/Modelo/src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
 package org.example;
 
+import java.util.Scanner;
 import io.github.ollama4j.OllamaAPI;
 import io.github.ollama4j.exceptions.OllamaBaseException;
 import io.github.ollama4j.models.response.OllamaResult;
-
 import java.io.IOException;
 
 public class Main {
@@ -11,12 +11,29 @@ public class Main {
     public static void main(String[] args) throws OllamaBaseException, IOException, InterruptedException {
         String host = "http://localhost:11434";
         String modelName = "qwen3:1.7b";
+        int opcao = 1;
 
         OllamaAPI ollamaAPI = new OllamaAPI(host);
         ollamaAPI.setRequestTimeoutSeconds(120);
-        OllamaResult result =
-                ollamaAPI.generate(modelName, "Quem descobriu o Brasil?", null);
 
-        System.out.println(result.getResponse());
+        System.out.println("Seja bem-vindo(a)! Sou uma IA, e estou animada para responder suas perguntas! :)");
+
+        while (opcao != 0) {
+            Scanner entrada = new Scanner(System.in);
+
+            System.out.println("Qual a sua pergunta:");
+            String pergunta = entrada.nextLine();
+
+            System.out.println("Pensando...");
+
+            OllamaResult result = ollamaAPI.generate(modelName, pergunta, null);
+            System.out.println(result.getResponse());
+
+            System.out.println("Você deseja continuar? (Sim = 1/Não = 0)");
+            Scanner ContOuPara = new Scanner(System.in);
+            opcao = ContOuPara.nextInt();
+        }
+
+        System.out.println("Até a próxima! :)");
     }
 }
